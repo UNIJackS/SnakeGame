ec713b3cc98c0d45728adeea1fc69c878a587983
Jack Scrivener <10jack222@gmail.com>
2023-06-29 10:19:56 +1200

    Added comments, allowed for resizing and added the exit button to prevent hanging

--- snake_v1.py
+++ snake_v1.py
@@ -2,21 +2,30 @@
 
 import pygame
 import time
+import sys
+from pygame.locals import QUIT
+
 
 intial_screen_width = 400
-
 intial_screen_height = 200
-
-#test comment for commit
 
 pygame.init()
 
-screen = pygame.display.set_mode((intial_screen_width,intial_screen_height))
+#Makes the inital page and sets its height and width to their intial values  and allows it to be resized
+screen = pygame.display.set_mode((intial_screen_width,intial_screen_height), pygame.RESIZABLE)
 
+#inports the awatapu logo and displays is as the icon of the program 
 game_icon = pygame.image.load('snake_icon.png')
 pygame.display.set_icon(game_icon)
 
-time.sleep(50)
+
+#Infinte loop that loops until the exit button is pressed in the window 
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 
 
 quit()
